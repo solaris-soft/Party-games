0c2a6a1f3a293b5b9654718e4e7bde2ef29db9bf
joshuamURD <jmacmaster9@gmail.com>
2025-05-06 19:18:54 +0800

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.46.0
      project name = party-app-frontend
      framework = svelte
      framework cli = sv@0.8.3
      package manager = bun@1.2.8
      wrangler = wrangler@4.14.0
      git = 2.48.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
-})
+	plugins: [sveltekit()]
+});
