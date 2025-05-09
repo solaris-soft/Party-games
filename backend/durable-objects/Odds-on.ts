da237402be0cffb1497e46850e0c287b9f31bdb8
joshuamURD <jmacmaster9@gmail.com>
2025-05-09 23:39:39 +0800

    Adding expected data for Odds-on

--- backend/durable-objects/Odds-on.ts
+++ backend/durable-objects/Odds-on.ts
@@ -1,5 +1,26 @@
 import { DurableObject } from 'cloudflare:workers';
 
+type Player = {
+  id: string;
+  name: string;
+  ready: boolean;
+  questions: Question[];
+  ws?: WebSocket;
+}
+
+type Question = {
+  id: string;
+  question: string;
+  answer: string;
+}
+
+type Room = {
+  id: string;
+  players: Player[];
+  questions: Question[];
+}
+
+
 export class OddsOn extends DurableObject {
   constructor(state: DurableObjectState, env: Env) {
     super(state, env);
